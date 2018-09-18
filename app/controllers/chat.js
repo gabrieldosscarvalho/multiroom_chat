b26afce5198277273c5f45883d8f4db33fda9d9a
Gabriel Carvalho <gabriel.dossantoscarvalho@hotmail.com>
2018-09-17 23:02:45 -0300

    - Aula 62

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat	= function( application, request, response ){
+	let dadosForm	= request.body;
+
+	request.check('apelido', 'Nome ou apelido é obrigatorio').notEmpty();
+	request.check('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+	let erros		= request.validationErrors();
+
+	if( erros ) {
+		response.render('index', {validacao: erros});
+		return;
+	}
+
 	response.render('chat');
 };
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,9 @@
 module.exports	= (application) => {
-	application.get('/chat', (request, response) => {
+	application.post('/chat', (request, response) => {
 		application.app.controllers.chat.iniciaChat( application, request, response );
 	});
 
-	application.post('/chat', (request, response) => {
+	application.get('/chat', (request, response) => {
 		application.app.controllers.chat.iniciaChat( application, request, response );
 	});
 };
