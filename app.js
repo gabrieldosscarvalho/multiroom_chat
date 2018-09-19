41414b1444b331485ea978eeee4dcf55571c1385
Gabriel Carvalho <gabriel.dossantoscarvalho@hotmail.com>
2018-09-18 21:59:16 -0300

    - Aula 65 - Integrando o Socket.io ao servidor

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 /* importar as configuracoes do servidor */
-var app = require('./config/server');
+var app		= require('./config/server');
 
-/* parametrizar a porta de escuta */
-app.listen(80, () => {
+/* parametrizar a porta de escuta - HTTP */
+var server	= app.listen(80, () => {
 	console.log('Servidor online.');
 });
+
+// Socket
+require('socket.io').listen(server);
